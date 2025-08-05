f880d4ffcdf6b37c28275e2e3461f95372b037c9
Mark Elliot <markelliot@users.noreply.github.com>
2025-08-05 03:26:02 -0400

    Update publishing for multi-project publish

--- result-test-utils/build.gradle.kts
+++ result-test-utils/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     `java-library`
+    `maven-publish`
+    `signing`
 }
 
 dependencies {
@@ -19,4 +21,50 @@ tasks.test {
 java {
     withJavadocJar()
     withSourcesJar()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+            suppressPomMetadataWarningsFor("javadocElements")
+            pom {
+                name.set("result")
+                description.set("A Rust-inspired success and error container.")
+                url.set("https://github.com/markelliot/result")
+                licenses {
+                    license {
+                        name.set("Apache License, Version 2.0")
+                        url.set("https://www.apache.org/licenses/LICENSE-2.0")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("markelliot")
+                        name.set("Mark Elliot")
+                        email.set("markelliot@users.noreply.github.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:https://github.com/markelliot/result.git")
+                    developerConnection.set("scm:git:https://github.com/markelliot/result.git")
+                    url.set("https://github.com/markelliot/result")
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            name = "staging"
+            url = uri(rootProject.layout.buildDirectory.dir("staging-deploy"))
+        }
+    }
+}
+
+configure<SigningExtension> {
+    val key = System.getenv("SIGNING_SECRET_KEY")
+    val password = System.getenv("SIGNING_PASSWORD")
+    val publishing: PublishingExtension by project
+    useInMemoryPgpKeys(key, password)
+    sign(publishing.publications)
 }
